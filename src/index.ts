bb02a3bc737b7140c5ad1444d8475f6a3281004a
Senior Sedo <145229171+Senior-Sedo@users.noreply.github.com>
2024-11-21 13:39:46 +0100

    \ the . & the - in the pattern + minifying

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-const seniorEMAIL = (email: string): boolean => /^[A-Za-z]+([^A-Za-z0-9]?[A-Za-z0-9]+)*@[A-Za-z]+([.-]?[A-Za-z0-9]+)*\.[A-Za-z]{2,}$/i.test(email);
+const seniorEMAIL = (email: string): boolean => /^[A-Za-z]+([^A-Za-z0-9]?[A-Za-z0-9]+)*@[A-Za-z]+([\.\-]?[A-Za-z0-9]+)*\.[A-Za-z]{2,}$/i.test(email);
 export default seniorEMAIL;
