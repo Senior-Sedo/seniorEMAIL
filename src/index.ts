d07f770041c2fe89d5d0b3c0561411b872248007
Senior Sedo <145229171+Senior-Sedo@users.noreply.github.com>
2024-11-27 16:33:28 +0100

    Added JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
-const seniorEMAIL = (email: string): boolean => /^[A-Za-z]+([^A-Za-z0-9]?[A-Za-z0-9]+)*@[A-Za-z]+([\.\-]?[A-Za-z0-9]+)*\.[A-Za-z]{2,}$/i.test(email);
+/**
+ * Validate an email => true | false.
+ * @param {string} email - required
+ * @returns {boolean} true | false
+ * @throws Error if the email is not a string
+ * 
+ * @example
+ * import seniorEMAIL from "@ssts/email";
+ * const isEmail: boolean = seniorEMAIL("example@gmail.com");
+ * 
+ * console.log(isEmail);
+ * // Log: true
+ */
+const seniorEMAIL = (email: string): boolean => {
+  if (typeof email !== "string") throw new TypeError("email must be a string");
+
+  return /^[A-Za-z]+([^A-Za-z0-9]?[A-Za-z0-9]+)*@[A-Za-z]+([\.\-]?[A-Za-z0-9]+)*\.[A-Za-z]{2,}$/i.test(email);
+};
+
+declare global {
+  interface Window {
+    ssts: {
+      seniorEMAIL?: typeof seniorEMAIL;
+    };
+  }
+}
+
+if (window != undefined) {
+  window.ssts = {};
+  window.ssts.seniorEMAIL = seniorEMAIL;
+}
+
 export default seniorEMAIL;
